597e92e609aedf43a2630de551637dc90a13a883
Nuga25 <osinugaifeoluwa1@gmail.com>
2024-12-10 09:48:08 +0100

    cleaned up code

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ function gameBoard() {
     const boardWithCellValues = board.map((row) =>
       row.map((cell) => cell.getValue())
     );
-    console.log(boardWithCellValues);
   };
 
   return { getBoard, dropToken, printBoard };
@@ -63,7 +62,6 @@ function GameController(
 
   const printNewRound = () => {
     board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn.`);
   };
 
   //function to handle winning logic
